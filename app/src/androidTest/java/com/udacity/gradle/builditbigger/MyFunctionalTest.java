0fa6a4d24c8b65f97de70806abd255b666110e06
Aya Salama <ayoyasalama@gmail.com>
2017-06-30 22:53:51 +0200

    Review mendings

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/MyFunctionalTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/MyFunctionalTest.java
@@ -11,6 +11,7 @@ import org.junit.runner.RunWith;
 import java.util.concurrent.ExecutionException;
 
 import static android.support.test.InstrumentationRegistry.getContext;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
 
@@ -23,13 +24,13 @@ public class MyFunctionalTest {
 
     @Test
     public void testIsJokeEmpty() throws InterruptedException, ExecutionException {
-
         String result = null;
         EndpointsAsyncTask endpointsAsyncTask
                 = new EndpointsAsyncTask(getContext(), null, null);
         endpointsAsyncTask.execute(new Pair<Context, String>(null, "Ay Haga"));
         result = endpointsAsyncTask.get();
         assertNotNull(result);
+        assertFalse(result.isEmpty());
 
     }
 }
---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,58 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.util.Pair;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+import com.example.JokesClass;
+import com.fleek.www.jokesuilibrary.JokeDisplayingActivity;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment
+        extends Fragment implements EndpointsAsyncTask.OnTaskCompleted {
+
+    private ProgressBar progressBar;
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        progressBar = (ProgressBar) root.findViewById(R.id.progress);
+
+        Button button = (Button) root.findViewById(R.id.bn_tell_joke);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                JokesClass jokesClass = new JokesClass();
+                Pair pair = new Pair(getActivity(), jokesClass.getAJoke());
+                new EndpointsAsyncTask(getActivity(), progressBar, MainActivityFragment.this).execute(pair);
+            }
+        });
+
+
+        return root;
+    }
+
+
+    @Override
+    public void onTaskCompleted(String joke) {
+        Intent intent = new Intent(getActivity(), JokeDisplayingActivity.class);
+        intent.putExtra("joke", joke);
+        startActivity(intent);
+    }
+}
